e01eff5630eb42f5c63d83526c26332df3645422
pauloariell <pauloariell@live.com>
2020-12-07 23:54:13 -0300

    Conectando a API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,56 +1,65 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/github-logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
-// React.FC = React Function
+interface Repository {
+  full_name: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+  description: string;
+  html_url: string;
+}
 
+// React.FC = React Function
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/11367212?s=460&u=cd32cf085c826c5bd34071f148cb0170ba2359f0&v=4"
-            alt="Paulo Chaves"
-          />
-          <div>
-            <strong>pauloarill/etielengenharia</strong>
-            <p>This project was generated with Angular CLI version 9.1.1.</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/11367212?s=460&u=cd32cf085c826c5bd34071f148cb0170ba2359f0&v=4"
-            alt="Paulo Chaves"
-          />
-          <div>
-            <strong>pauloarill/etielengenharia</strong>
-            <p>This project was generated with Angular CLI version 9.1.1.</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/11367212?s=460&u=cd32cf085c826c5bd34071f148cb0170ba2359f0&v=4"
-            alt="Paulo Chaves"
-          />
-          <div>
-            <strong>pauloarill/etielengenharia</strong>
-            <p>This project was generated with Angular CLI version 9.1.1.</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href={repository.html_url}>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
